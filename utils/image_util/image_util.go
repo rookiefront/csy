20e093b3f903c0acf9e58cf3a9650de166eeaad3
ck996 <you@example.com>
2026-04-06 15:34:40 +0800

    update

--- utils/image_util/image_util.go
+++ utils/image_util/image_util.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 	"image"
 	"image/png"
+	"regexp"
+	"strings"
 
 	"github.com/disintegration/imaging"
 )
@@ -31,4 +33,50 @@ func ToBase64Str(img image.Image) (string, error) {
 	base64Str := base64.StdEncoding.EncodeToString(buf.Bytes())
 	// 添加 Data URL 前缀
 	return "data:image/png;base64," + base64Str, nil
+}
+
+func Base64toImage(base64Str string) (image.Image, error) {
+	base64Str = Base64RemoveFirstTag(base64Str)
+	// 解码 Base64 字符串
+	decoded, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return nil, err
+	}
+
+	// 创建图像对象
+	img, err := imaging.Decode(bytes.NewReader(decoded))
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+func base64ReplaceEncode(data []byte) string {
+	str := base64.StdEncoding.EncodeToString(data)
+	str = strings.Replace(str, "+", "-", -1)
+	str = strings.Replace(str, "/", "_", -1)
+	str = strings.Replace(str, "=", "", -1)
+	return str
+}
+
+func base64ReplaceDecode(str string) ([]byte, error) {
+	str = strings.Replace(str, "-", "+", -1)
+	str = strings.Replace(str, "_", "/", -1)
+	for len(str)%4 != 0 {
+		str += "="
+	}
+	return base64.StdEncoding.DecodeString(str)
+}
+
+func Base64RemoveFirstTag(base64Str string) string {
+
+	// 使用正则表达式提取 Base64 字符串部分
+	re := regexp.MustCompile(`^data:image\/\w+;base64,(.+)`)
+	matches := re.FindStringSubmatch(base64Str)
+
+	if len(matches) > 1 {
+		base64Str = matches[1]
+	}
+
+	return base64Str
 }

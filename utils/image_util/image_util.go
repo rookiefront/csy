51a647734d421070e099af04b200a10b9e60f83c
ck996 <you@example.com>
2026-04-06 15:06:59 +0800

    update

---
+++ utils/image_util/image_util.go
@@ -0,0 +1,33 @@
+package csy_image_util
+
+import (
+	"bytes"
+	"encoding/base64"
+	"image"
+	"image/png"
+
+	"github.com/disintegration/imaging"
+)
+
+func Resize(img *image.RGBA, width int) (resizedImg image.Image) {
+	if width > 0 {
+		bounds := img.Bounds()
+		originalWidth := bounds.Dx()
+		originalHeight := bounds.Dy()
+		newHeight := int(float64(originalHeight) * (float64(width) / float64(originalWidth)))
+		resizedImg = imaging.Resize(img, width, newHeight, imaging.Lanczos)
+	} else {
+		resizedImg = img
+	}
+	return resizedImg
+}
+
+func ToBase64Str(img image.Image) (string, error) {
+	// 编码为 PNG
+	var buf bytes.Buffer
+	if err := png.Encode(&buf, img); err != nil {
+		return "", err
+	}
+	base64Str := base64.StdEncoding.EncodeToString(buf.Bytes())
+	return base64Str, nil
+}
